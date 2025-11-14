7871fb6edf1b37abe0305c72ffd098aa6a18a07b
Aysha Tahoor <2ab23cs015@anjuman.edu.in>
2025-11-14 23:40:07 +0530

    Add message and say hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,5 +3,6 @@ use namespace std;
 int main() {
   cout<<"Namaste Duniya"<<endl;
   cout<<"Every Expert was once a beginner so keep coding!"<<endl;
+  cout<<"You just need to try it out Aysha, unless and until you do its never gonna happen"<<endl;
   return 0;
 }
