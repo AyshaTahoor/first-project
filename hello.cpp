9fb0b42cbb4cb0d9bd73d116fd2834908b85d1a6
Aysha Tahoor <2ab23cs015@anjuman.edu.in>
2026-01-03 23:28:26 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,7 @@
 #include <iostream.h>
 use namespace std;
 int main() {
-  cout<<"Namaste Duniya"<<endl;
+  cout<<"Hello, Namaste Duniya"<<endl;
   cout<<"Every Expert was once a beginner so keep coding!"<<endl;
   cout<<"You just need to try it out Aysha, unless and until you do it is never gonna happen"<<endl;
   return 0;
